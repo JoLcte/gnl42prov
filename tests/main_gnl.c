a32668afd6e9d65c9c9d9ff680ddb1ee8691c390
Johanna LECOMTE <jlecomte@e2r3p16.42.fr>
2021-01-27 15:17:55 +0100

    free is better

--- tests/main_gnl.c
+++ tests/main_gnl.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "../get_next_line.h"
+#include "get_next_line.h"
 #include <fcntl.h>
 
 int	main(void)
@@ -7,9 +7,17 @@ int	main(void)
 	char *s;
 	int fd;
 	int i = 1;
+	int ans;
 
 	fd = open("testfile.txt", O_RDONLY);
-	while (get_next_line(fd, &s)  > 0)
-		printf("%d)%s|\n", i++, s); 
+	while ((ans = get_next_line(fd, &s))  > 0)
+	{
+		printf("returned |%d|\n", ans);
+		printf("%d)%s|\n", i++, s);
+		free(s);
+	}
+		printf("returned |%d|\n", ans);
+		printf("%d)%s|\n", i, s);
+	free(s);
 	return (0);
 }
